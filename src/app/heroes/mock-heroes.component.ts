cb1cc4fb390c18a93fcfbc53f55f2704be4b0ca4
Antoine M <antoine-moreno34@hotmail.fr>
2019-12-11 21:55:44 +0100

    pek code

---
+++ src/app/heroes/mock-heroes.component.ts
@@ -0,0 +1,24 @@
+import { Hero } from './heroes';
+
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: "Ger",
+        pgm: 99,
+        skill:80,
+    },
+    {
+        id: 2,
+        name: "Kevin",
+        pgm: 85,
+        skill:80,
+    },
+    {
+        id: 3,
+        name: "Max",
+        pgm: 99,
+        skill:80,
+    },
+   
+
+];
